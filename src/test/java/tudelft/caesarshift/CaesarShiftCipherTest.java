d8bb7c2cb345d30289ebee072b71d595a5c4be1a
med4it <mohammed.aithaddou@gmail.com>
2019-11-24 23:28:10 +0100

    Reaching 100% line coverage on Caesar Cipher unit tests

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -8,7 +8,7 @@ public class CaesarShiftCipherTest {
     @ParameterizedTest(name = "{0} [{1} + {2}] => {3}")
     @CsvSource({
             "'space', ' ', 0, ' '",
-            "'one char in a-z', 'a', 1, 'b'",
+            "'one char in a-z', 'z', 1, 'a'",
             "'invalid pattern', '^ssldfj', 0, 'invalid'",
             "'message with inPoint-shift', 'abc', 1, 'bcd'",
             "'message with outPoint-shift', 'abc', 28, 'cde'",
