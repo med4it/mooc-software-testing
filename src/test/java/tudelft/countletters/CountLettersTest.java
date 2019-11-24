23bf522ad57d26e96556919bd719a133d1c61037
med4it <mohammed.aithaddou@gmail.com>
2019-11-24 23:29:23 +0100

    CountLetters unit tests with 100% coverage on Line, Branch and Branch conditions & fixing bug

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -2,19 +2,23 @@ package tudelft.countletters;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CountLettersTest {
 
-    @Test
-    public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
-        Assertions.assertEquals(2, words);
-    }
-
-    @Test
-    public void lastWordDoesNotMatch() {
-        int words = new CountLetters().count("cats|dog");
-        Assertions.assertEquals(1, words);
+    @ParameterizedTest
+    @CsvSource({
+        "'one word ending with s', 'sas', 1",
+        "'one word ending with r', 'near', 1",
+        "'one word does not match', 'sa', 0",
+        "'two words ending with s', 'cats|dogs', 2",
+        "'two words ending with r', 'car|war', 2",
+        "'two words does not match', 'case|wall', 0",
+    })
+    public void testAlgorithm(String partition, String str, int expected) {
+        int words = new CountLetters().count(str);
+        Assertions.assertEquals(expected, words);
     }
 
 }
