291f53870e72329fdd3eb41b18194d3670d9b2c3
med4it <mohammed.aithaddou@gmail.com>
2019-11-24 20:51:43 +0100

    CaesarCipher unit tests to fix bug

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -11,9 +11,12 @@ public class CaesarShiftCipher {
 
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
-           
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
+
+            if(currentChar == ' '){
+                sb.append(currentChar);
+                continue;
+            }
+            else if ((currentChar > 'z' || currentChar < 'a')) {
                 return "invalid";
             } else if ((char) (currentChar + shift) > 'z') {
                 currentChar = (char) (currentChar - 26);
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,26 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class CaesarShiftCipherTest {
+    @ParameterizedTest(name = "{0} [{1} + {2}] => {3}")
+    @CsvSource({
+            "'space', ' ', 0, ' '",
+            "'one char in a-z', 'a', 1, 'b'",
+            "'invalid pattern', '^ssldfj', 0, 'invalid'",
+            "'message with inPoint-shift', 'abc', 1, 'bcd'",
+            "'message with outPoint-shift', 'abc', 28, 'cde'",
+            "'message with negative outPoint-shift', 'abc', -1, 'zab'",
+            "'message with onPoint-shift', 'abc', 26, 'abc",
+            "'message with offPoint-shift', 'abc', 27, 'bcd",
+            "'message with spaces', 'a b c', 1, 'b c d'"
+    })
+
+    void testAlgorithm(String partition, String message, int shift, String expectedResult){
+        CaesarShiftCipher crypt = new CaesarShiftCipher();
+        String result = crypt.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
